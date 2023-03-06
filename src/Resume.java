1b3639f8043deda9f070b6e85546ba312d7de570
gennadykudryavtsev <gennadykudryavtsev@gmail.com>
2023-03-06 20:23:23 +0300

    New commit

--- src/Resume.java
+++ src/Resume.java
@@ -1,13 +1,13 @@
 /**
  * Initial resume class
  */
-public class Resume {
-
-    // Unique identifier
-    String uuid;
-
-    @Override
-    public String toString() {
-        return uuid;
-    }
+//public class Resume {
+//
+//    // Unique identifier
+//    String uuid;
+//
+//    @Override
+//    public String toString() {
+//        return uuid;
+//    }
 }
